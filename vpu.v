8e7032f6fe7d3cd9858580750548957119a3e5d1
lxaw <whalenlex@gmail.com>
2024-05-25 10:11:55 -0400

    comments

--- vpu.v
+++ vpu.v
@@ -1,133 +1,233 @@
-`timescale 1ns / 1ps
-
-// fields of IR
-`define oper_type IR[31:27]
-`define rdst IR[26:22]
-`define rsrc1 IR[21:17]
-`define imm_mode IR[16]
-`define rsrc2 IR[15:11]
-`define isrc IR[15:0]
-
-// arith ops
-`define movsgpr 5'b00000
-`define mov 5'b00001
-`define add 5'b00010
-`define sub 5'b00011
-`define mul 5'b00100
-
-// logical ops
-// we use ror, since 'or' is already a keyword
-`define ror 5'b00101
-`define rand 5'b00110
-`define rxor 5'b00111
-`define rxnor 5'b01000
-`define rnand 5'b01001
-`define rnor 5'b01010
-`define rnot 5'b01011
-
-module top();
-    reg [31:0] IR; // instruction register
-    // fields: [31:27]-[26:22]-[21:17]-[16] -[15:11]- [10:0]
-    // oper rdest rsrc1 modesel rsrc2 unusued
-    // oper rdest rsrc1 modesel immediate
-
-    // note that bit 16 tells us if we shall be using the immediate or rsrc2
-    // this is not great design, but simple enough to implement
-    // our registers can only store 16 bit in this architecture
-    reg [31:0] GPR [31:0]; // general purpose register
-    // gpr[0] ... gpr[31]
-    // since we chose 5 bits for rdest, we need 32 (2^5) gprs
-    reg [31:0] SGPR; // mul_out(31:16) MSB 16 bit MSB 16 bit MSB 16 bit MSB 16 bit
-    // RDST = mul_out(15:0) LSB 16 bit
-    reg [63:0] mul_res;
-
-    always @(*) // asterisk indicates that as soon as any value of a variable changes, we re-evaluate
+`timescale 1ns / 1ps // specify time unit and precision
+    
+///////////fields of IR // define fields of the instruction register (IR)
+`define oper_type IR[31:27] // operation type
+`define rdst      IR[26:22] // destination register
+`define rsrc1     IR[21:17] // source register 1
+`define imm_mode  IR[16]    // immediate mode
+`define rsrc2     IR[15:11] // source register 2
+`define isrc      IR[15:0]  // immediate value or source register 2 (depending on imm_mode)
+    
+    
+////////////////arithmetic operation // define operation codes for arithmetic operations
+`define movsgpr        5'b00000 // move from special register to general purpose register
+`define mov            5'b00001 // move operation
+`define add            5'b00010 // addition
+`define sub            5'b00011 // subtraction
+`define mul            5'b00100 // multiplication
+    
+////////////////logical operations : and or xor xnor nand nor not // define operation codes for logical operations
+    
+`define ror            5'b00101 // bitwise OR
+`define rand           5'b00110 // bitwise AND
+`define rxor           5'b00111 // bitwise XOR
+`define rxnor          5'b01000 // bitwise XNOR
+`define rnand          5'b01001 // bitwise NAND
+`define rnor           5'b01010 // bitwise NOR
+`define rnot           5'b01011 // bitwise NOT
+    
+    
+module top(); // define top-level module
+    
+    
+    
+    
+    
+    
+reg [31:0] IR; // instruction register
+                            
+reg [15:0] GPR [31:0] ; // general purpose registers (32 registers of 16 bits each)
+    
+    
+    
+reg [15:0] SGPR ; // special register for storing most significant bits of multiplication result
+    
+reg [31:0] mul_res; // store full 32-bit result of multiplication
+    
+    
+    
+always@(*) // always block, triggered on any signal change
+begin
+case(`oper_type) // case statement based on operation type
+///////////////////////////////
+`movsgpr: begin // move from special register to general purpose register
+    
+    GPR[`rdst] = SGPR; // copy value from special register to destination register
+    
+end
+    
+/////////////////////////////////
+`mov : begin // move operation
+    if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = `isrc; // copy immediate value to destination register
+    else
+        GPR[`rdst]   = GPR[`rsrc1]; // copy value from source register 1 to destination register
+end
+    
+////////////////////////////////////////////////////
+    
+`add : begin // addition
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]   = GPR[`rsrc1] + `isrc; // add source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] + GPR[`rsrc2]; // add source register 1 and source register 2, store in destination register
+end
+    
+/////////////////////////////////////////////////////////
+    
+`sub : begin // subtraction
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = GPR[`rsrc1] - `isrc; // subtract immediate value from source register 1, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] - GPR[`rsrc2]; // subtract source register 2 from source register 1, store in destination register
+end
+    
+/////////////////////////////////////////////////////////////
+    
+`mul : begin // multiplication
+        if(`imm_mode) // if immediate mode
+        mul_res   = GPR[`rsrc1] * `isrc; // multiply source register 1 and immediate value
+        else
+        mul_res   = GPR[`rsrc1] * GPR[`rsrc2]; // multiply source register 1 and source register 2
+        
+        GPR[`rdst]   =  mul_res[15:0]; // store least significant 16 bits of result in destination register
+        SGPR         =  mul_res[31:16]; // store most significant 16 bits of result in special register
+end
+    
+///////////////////////////////////////////////////////////// bitwise or
+    
+`ror : begin // bitwise OR
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = GPR[`rsrc1] | `isrc; // bitwise OR source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] | GPR[`rsrc2]; // bitwise OR source register 1 and source register 2, store in destination register
+end
+    
+////////////////////////////////////////////////////////////bitwise and
+    
+`rand : begin // bitwise AND
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = GPR[`rsrc1] & `isrc; // bitwise AND source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] & GPR[`rsrc2]; // bitwise AND source register 1 and source register 2, store in destination register
+end
+    
+//////////////////////////////////////////////////////////// bitwise xor
+    
+`rxor : begin // bitwise XOR
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = GPR[`rsrc1] ^ `isrc; // bitwise XOR source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] ^ GPR[`rsrc2]; // bitwise XOR source register 1 and source register 2, store in destination register
+end
+    
+//////////////////////////////////////////////////////////// bitwise xnor
+    
+`rxnor : begin // bitwise XNOR
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = GPR[`rsrc1] ~^ `isrc; // bitwise XNOR source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = GPR[`rsrc1] ~^ GPR[`rsrc2]; // bitwise XNOR source register 1 and source register 2, store in destination register
+end
+    
+//////////////////////////////////////////////////////////// bitwisw nand
+    
+`rnand : begin // bitwise NAND
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = ~(GPR[`rsrc1] & `isrc); // bitwise NAND source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = ~(GPR[`rsrc1] & GPR[`rsrc2]); // bitwise NAND source register 1 and source register 2, store in destination register
+end
+    
+////////////////////////////////////////////////////////////bitwise nor
+    
+`rnor : begin // bitwise NOR
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = ~(GPR[`rsrc1] | `isrc); // bitwise NOR source register 1 and immediate value, store in destination register
+        else
+        GPR[`rdst]   = ~(GPR[`rsrc1] | GPR[`rsrc2]); // bitwise NOR source register 1 and source register 2, store in destination register
+end
+    
+////////////////////////////////////////////////////////////not
+    
+`rnot : begin // bitwise NOT
+        if(`imm_mode) // if immediate mode
+        GPR[`rdst]  = ~(`isrc); // bitwise NOT immediate value, store in destination register
+        else
+        GPR[`rdst]   = ~(GPR[`rsrc1]); // bitwise NOT source register 1, store in destination register
+end
+    
+////////////////////////////////////////////////////////////
+    
+endcase
+end
+    
+    
+    
+///////////////////////logic for condition flag // logic for setting condition flags (sign, zero, overflow, carry)
+reg sign = 0, zero = 0, overflow = 0, carry = 0;
+reg [16:0] temp_sum;
+    
+always@(*)
+begin
+    
+/////////////////sign bit
+if(`oper_type == `mul)
+    sign = SGPR[15];
+else
+    sign = GPR[`rdst][15];
+    
+////////////////carry bit
+    
+if(`oper_type == `add)
     begin
-        // do a case statement on operation type
-        case (`oper_type)
-            `movsgpr: 
-                begin
-                    GPR[`rdst] = SGPR; // if movsgpr, need to store the value of SGPR into GPR
-                end
-            `mov: 
-                begin
-                    if (`imm_mode)
-                        GPR[`rdst] = `isrc; // if imm_mode 1, then need an immediate
-                    else
-                        GPR[`rdst] = GPR[`rsrc1];
-                end
-            `add: 
-                begin
-                    if (`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] + `isrc;
-                    else
-                        GPR[`rdst] = GPR[`rsrc1] + GPR[`rsrc2];
-                end
-            `sub: 
-                begin
-                    if (`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] - `isrc;
-                    else
-                        GPR[`rdst] = GPR[`rsrc1] - GPR[`rsrc2];
-                end
-            `mul: 
-                begin
-                    if (`imm_mode)
-                        mul_res = GPR[`rsrc2] * `isrc;
-                    else
-                        mul_res = GPR[`rsrc1] * GPR[`rsrc2];
-                    GPR[`rdst] = mul_res[31:0];
-                    SGPR = mul_res[63:32];
-                end
-            `ror: 
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] | `isrc;
-                    else
-                        GPR[`rdst] = GPR[`rsrc1] | GPR[`rsrc2];
-                end
-            `rand:
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] & `isrc;
-                    else
-                        GPR[`rdst] = GPR[`rsrc1] & GPR[`rsrc2];
-                end
-            `rxor: 
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] ^ `isrc;
-                    else   
-                        GPR[`rdst] = GPR[`rsrc1] ^ GPR[`rsrc2];
-                end
-            `rxnor:
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = GPR[`rsrc1] ~^ `isrc;
-                    else
-                        GPR[`rdst] = GPR[`rsrc1] ~^ GPR[`rsrc2];
-                end
-            `rnand:
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = ~(GPR[`rsrc1] & `isrc);
-                    else
-                        GPR[`rdst] = ~(GPR[`rsrc1] & GPR[`rsrc2]);
-                end
-            `rnor:
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = ~(GPR[`rsrc1] | `isrc);
-                    else
-                        GPR[`rdst] = ~(GPR[`rsrc1] | GPR[`rsrc2]);
-                end
-            `rnot:
-                begin
-                    if(`imm_mode)
-                        GPR[`rdst] = ~(`isrc);
-                    else
-                        GPR[`rdst] = ~(GPR[`rsrc1]);
-                end
-        endcase
+        if(`imm_mode)
+            begin
+            temp_sum = GPR[`rsrc1] + `isrc;
+            carry    = temp_sum[16]; 
+            end
+        else
+            begin
+            temp_sum = GPR[`rsrc1] + GPR[`rsrc2];
+            carry    = temp_sum[16]; 
+            end   end
+    else
+    begin
+        carry  = 1'b0;
     end
+    
+///////////////////// zero bit
+if(`oper_type == `mul)
+    zero =  ~((|SGPR[15:0]) | (|GPR[`rdst]));
+else
+    zero =  ~(|GPR[`rdst]); 
+    
+    
+//////////////////////overflow bit
+    
+if(`oper_type == `add)
+        begin
+        if(`imm_mode)
+            overflow = ( (~GPR[`rsrc1][15] & ~IR[15] & GPR[`rdst][15] ) | (GPR[`rsrc1][15] & IR[15] & ~GPR[`rdst][15]) );
+        else
+            overflow = ( (~GPR[`rsrc1][15] & ~GPR[`rsrc2][15] & GPR[`rdst][15]) | (GPR[`rsrc1][15] & GPR[`rsrc2][15] & ~GPR[`rdst][15]));
+        end
+    else if(`oper_type == `sub)
+    begin
+        if(`imm_mode)
+            overflow = ( (~GPR[`rsrc1][15] & IR[15] & GPR[`rdst][15] ) | (GPR[`rsrc1][15] & ~IR[15] & ~GPR[`rdst][15]) );
+        else
+            overflow = ( (~GPR[`rsrc1][15] & GPR[`rsrc2][15] & GPR[`rdst][15]) | (GPR[`rsrc1][15] & ~GPR[`rsrc2][15] & ~GPR[`rdst][15]));
+    end 
+    else
+        begin
+        overflow = 1'b0;
+        end
+    
+end
+    
+    
+    
 endmodule
+    
+////////////////////////////////////////////////////////////////////////////
--- vpu_tb.v
+++ vpu_tb.v
@@ -1,119 +1,131 @@
-// testbench for vpu
-module tb;
-    integer i = 0; // keep track of element number
+    module tb;
+     
+     
+    integer i = 0;
+     
     top dut();
-
-    // update all gpr to some known value, we choose 2
+     
+    ///////////////updating value of all GPR to 2
     initial begin
-        for (i = 0; i < 32; i = i + 1) begin
-            dut.GPR[i] = 2;
-        end
+    for( i = 0; i < 32; i = i + 1)
+    begin
+    dut.GPR[i] = 2;
     end
-
+    end
+     
+     
+     
     initial begin
-        $display("-----------------------------------------------------------------");
-        dut.IR = 0;
-        dut.`imm_mode = 1;
-        dut.`oper_type = 2;
-        dut.`rsrc1 = 2;///gpr[2] = 2
-        dut.`rdst  = 0;///gpr[0]
-        dut.`isrc = 4;
-        #10;
-        $display("OP:ADI Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[2], dut.`isrc, dut.GPR[0]);
-        $display("-----------------------------------------------------------------");
-        ////////////register add op
-        dut.IR = 0;
-        dut.`imm_mode = 0;
-        dut.`oper_type = 2;
-        dut.`rsrc1 = 4;
-        dut.`rsrc2 = 5;
-        dut.`rdst  = 0;
-        #10;
-        $display("OP:ADD Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[4], dut.GPR[5], dut.GPR[0] );
-        $display("-----------------------------------------------------------------");
-        
-        //////////////////////immediate mov op
-        dut.IR = 0;
-        dut.`imm_mode = 1;
-        dut.`oper_type = 1;
-        dut.`rdst = 4;///gpr[4]
-        dut.`isrc = 55;
-        #10;
-        $display("OP:MOVI Rdst:%0d  imm_data:%0d",dut.GPR[4],dut.`isrc  );
-        $display("-----------------------------------------------------------------");
-        
-        //////////////////register mov
-        dut.IR = 0;
-        dut.`imm_mode = 0;
-        dut.`oper_type = 1;
-        dut.`rdst = 4;
-        dut.`rsrc1 = 7;//gpr[7]
-        #10;
-        $display("OP:MOV Rdst:%0d  Rsrc1:%0d",dut.GPR[4],dut.GPR[7] );
-        $display("-----------------------------------------------------------------");
-        
-        //////////////////////logical and imm
-        dut.IR = 0;
-        dut.`imm_mode = 1;
-        dut.`oper_type = 6;
-        dut.`rdst = 4;
-        dut.`rsrc1 = 7;//gpr[7]
-        dut.`isrc = 56;
-        #10;
-        $display("OP:ANDI Rdst:%8b  Rsrc1:%8b imm_d :%8b",dut.GPR[4],dut.GPR[7],dut.`isrc );
-        $display("-----------------------------------------------------------------");
-        
-        ///////////////////logical or imm
-        dut.IR = 0;
-        dut.`imm_mode = 1;
-        dut.`oper_type = 7;
-        dut.`rdst = 4;
-        dut.`rsrc1 = 7;//gpr[7]
-        dut.`isrc = 56;
-        #10;
-        $display("OP:XORI Rdst:%8b  Rsrc1:%8b imm_d :%8b",dut.GPR[4],dut.GPR[7],dut.`isrc );
-        $display("-----------------------------------------------------------------");
-        // immediate add op
-        $display("------------------------------");
-        dut.IR = 0;
-        dut.`imm_mode = 1; // immediate addressing mode
-        dut.`oper_type = 2; // addition code
-        dut.`rsrc1 = 2; // gpr[2] should have value of 2 in beginning
-        dut.`rdst = 0; // gpr[0]
-        dut.`isrc = 4;
-        #10
-        $display("OP:ADI Rsrc1: %0d Rsrc2:%0d Rdst:%0d", dut.GPR[2], dut.`isrc, dut.GPR[0]);
-        $display("------------------------------");
-
-        // register add op
-        dut.IR = 0;
-        dut.`imm_mode = 0;
-        dut.`oper_type = 2;
-        dut.`rsrc1 = 4;
-        dut.`rsrc2 = 5;
-        dut.`rdst = 0;
-        #10
-        $display("OP:ADD Rsrc1:%0d Rsrc2:%0d Rdst:%0d", dut.GPR[4], dut.GPR[5], dut.GPR[0]);
-        $display("------------------------------");
-
-        // immediate mov op
-        dut.IR = 0;
-        dut.`imm_mode = 1;
-        dut.`oper_type = 1; // val for move type is 1
-        dut.`rdst = 4; // GPR[4]
-        dut.`isrc = 55;
-        #10
-        $display("OP:MOVI Rdst:%0d imm_data:%0d", dut.GPR[4], dut.`isrc);
-        $display("------------------------------");
-
-        // register mov
-        dut.IR = 0;
-        dut.`imm_mode = 0;
-        dut.`oper_type = 1;
-        dut.`rdst = 4;
-        dut.`rsrc1 = 7;
-        #10
-        $display("OP:MOV Rdst:%0d Rsrc1:%0d", dut.GPR[4], dut.GPR[7]);
-        $display("------------------------------");
+    //////// immediate add op
+    $display("-----------------------------------------------------------------");
+    dut.IR = 0;
+    dut.`imm_mode = 1;
+    dut.`oper_type = 2;
+    dut.`rsrc1 = 2;///gpr[2] = 2
+    dut.`rdst  = 0;///gpr[0]
+    dut.`isrc = 4;
+    #10;
+    $display("OP:ADI Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[2], dut.`isrc, dut.GPR[0]);
+    $display("-----------------------------------------------------------------");
+    ////////////register add op
+    dut.IR = 0;
+    dut.`imm_mode = 0;
+    dut.`oper_type = 2;
+    dut.`rsrc1 = 4;
+    dut.`rsrc2 = 5;
+    dut.`rdst  = 0;
+    #10;
+    $display("OP:ADD Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[4], dut.GPR[5], dut.GPR[0] );
+    $display("-----------------------------------------------------------------");
+     
+    //////////////////////immediate mov op
+    dut.IR = 0;
+    dut.`imm_mode = 1;
+    dut.`oper_type = 1;
+    dut.`rdst = 4;///gpr[4]
+    dut.`isrc = 55;
+    #10;
+    $display("OP:MOVI Rdst:%0d  imm_data:%0d",dut.GPR[4],dut.`isrc  );
+    $display("-----------------------------------------------------------------");
+     
+    //////////////////register mov
+    dut.IR = 0;
+    dut.`imm_mode = 0;
+    dut.`oper_type = 1;
+    dut.`rdst = 4;
+    dut.`rsrc1 = 7;//gpr[7]
+    #10;
+    $display("OP:MOV Rdst:%0d  Rsrc1:%0d",dut.GPR[4],dut.GPR[7] );
+    $display("-----------------------------------------------------------------");
+     
+     
+     
+     
+     
+    //////////////////////logical and imm
+    dut.IR = 0;
+    dut.`imm_mode = 1;
+    dut.`oper_type = 6;
+    dut.`rdst = 4;
+    dut.`rsrc1 = 7;//gpr[7]
+    dut.`isrc = 56;
+    #10;
+    $display("OP:ANDI Rdst:%8b  Rsrc1:%8b imm_d :%8b",dut.GPR[4],dut.GPR[7],dut.`isrc );
+    $display("-----------------------------------------------------------------");
+     
+    ///////////////////logical xor imm
+    dut.IR = 0;
+    dut.`imm_mode = 1;
+    dut.`oper_type = 7;
+    dut.`rdst = 4;
+    dut.`rsrc1 = 7;//gpr[7]
+    dut.`isrc = 56;
+    #10;
+    $display("OP:XORI Rdst:%8b  Rsrc1:%8b imm_d :%8b",dut.GPR[4],dut.GPR[7],dut.`isrc );
+    $display("-----------------------------------------------------------------");
+     
+    /////////////////////////// zero flag
+    dut.IR  = 0;
+    dut.GPR[0] = 0;
+    dut.GPR[1] = 0; 
+    dut.`imm_mode = 0;
+    dut.`rsrc1 = 0;//gpr[0]
+    dut.`rsrc2 = 1;//gpr[1]
+    dut.`oper_type = 2;
+    dut.`rdst = 2;
+    #10;
+    $display("OP:Zero Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[0], dut.GPR[1], dut.GPR[2] );
+    $display("-----------------------------------------------------------------");
+     
+    //////////////////////////sign flag
+    dut.IR = 0;
+    dut.GPR[0] = 16'h8000; /////1000_0000_0000_0000
+    dut.GPR[1] = 0; 
+    dut.`imm_mode = 0;
+    dut.`rsrc1 = 0;//gpr[0]
+    dut.`rsrc2 = 1;//gpr[1]
+    dut.`oper_type = 2;
+    dut.`rdst = 2;
+    #10;
+    $display("OP:Sign Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[0], dut.GPR[1], dut.GPR[2] );
+    $display("-----------------------------------------------------------------");
+     
+    ////////////////////////carry flag
+    dut.IR = 0;
+    dut.GPR[0] = 16'h8000; /////1000_0000_0000_0000   <0
+    dut.GPR[1] = 16'h8002; /////1000_0000_0000_0010   <0
+    dut.`imm_mode = 0;
+    dut.`rsrc1 = 0;//gpr[0]
+    dut.`rsrc2 = 1;//gpr[1]
+    dut.`oper_type = 2;
+    dut.`rdst = 2;    //////// 0000_0000_0000_0010  >0
+    #10;
+     
+    $display("OP:Carry & Overflow Rsrc1:%0d  Rsrc2:%0d Rdst:%0d",dut.GPR[0], dut.GPR[1], dut.GPR[2] );
+    $display("-----------------------------------------------------------------");
+     
+    #20;
+    $finish;
     end
-endmodule
+     
+    endmodule
